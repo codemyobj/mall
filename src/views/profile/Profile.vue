93d7b4208486757caa4f5a066c2e4d9df59ac17f
codemyobj <459324704@qq.com>
2021-05-22 11:43:51 +0800

    完成项目

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -3,32 +3,106 @@
     <nav-bar class="nav-bar">
       <div slot="center">我的商城</div>
     </nav-bar>
+    <UserInfo />
 
-    <user-info />
+    <section class="account">
+      <div class="account-item">
+        <div class="number">
+          <span class="balance">0.00</span>元
+        </div>
+        <div class="account-info">我的余额</div>
+      </div>
+      <div class="account-item">
+        <div class="number">
+          <span class="balance">0</span>个
+        </div>
+        <div class="account-info">我的优惠</div>
+      </div>
+      <div class="account-item">
+        <div class="number">
+          <span class="balance">0</span>分
+        </div>
+        <div class="account-info">我的积分</div>
+      </div>
+    </section>
+
+    <UserList :list-data='orderList' class="order-list" />
+    <UserList :list-data='serviceList' class="service-list" />
   </div>
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar";
+  import NavBar from "components/common/navbar/NavBar";
+  import UserInfo from "./childComps/UserInfo";
+  import UserList from './childComps/UserList'
 
-import UserInfo from "./childComps/UserInfo";
-
-export default {
-  name: "Profile",
-  components: {
-    NavBar,
-    UserInfo,
-  },
-};
+  export default {
+    name: "Profile",
+    components: {
+      NavBar,
+      UserInfo,
+      UserList
+    },
+    data() {
+      return {
+        orderList: [
+          { icon: '#order', iconColor: '#ff8198', info: '我的消息' },
+          { icon: '#point', iconColor: '#fc7b53', info: '积分商城' },
+          { icon: '#vip', iconColor: '#ffc636', info: '会员卡' }
+        ],
+        serviceList: [
+          { icon: '#service', iconColor: '#ff8198', info: '我的购物车' },
+          { icon: '#download', iconColor: '#ff8198', info: '下载购物车APP' }
+        ]
+      }
+    }
+  };
 </script>
 
 <style scoped>
-#profile {
-  background-color: #f2f2f2;
-}
-.nav-bar {
-  background-color: var(--color-tint);
-  font-weight: 700;
-  color: #fff;
-}
+  #profile {
+    background-color: #f2f2f2;
+  }
+
+  .nav-bar {
+    background-color: var(--color-tint);
+    font-weight: 700;
+    color: #fff;
+  }
+
+  .account {
+    display: flex;
+  }
+
+  .account-item {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    background-color: #fff;
+    margin-right: 1px;
+    color: #666;
+    font-size: 13px;
+    padding: 18px;
+  }
+
+  .account-item:last-of-type {
+    margin-right: 0;
+  }
+
+  .account-item .balance {
+    font-size: 24px;
+    font-weight: 700;
+    color: #ff5f3e;
+  }
+
+  .account-info {
+    margin-top: 6px;
+  }
+
+  .order-list,
+  .service-list {
+    margin-top: 12px;
+  }
 </style>
--- src/views/profile/childComps/UserInfo.vue
+++ src/views/profile/childComps/UserInfo.vue
@@ -1,12 +1,83 @@
 <template>
-  <div id="user-info"></div>
+  <div id="user-info">
+    <a href="#" class="user-info_wrap">
+      <div class="left">
+        <div class="left-img">
+          <img src="@/assets/img/profile/avatar.svg" alt />
+        </div>
+        <div class="left-text">
+          <span>登录/注册</span>
+          <span class="shouji">
+            <img src="~@/assets/img/profile/sj.png" alt />
+            <div>暂无绑定手机号</div>
+          </span>
+        </div>
+      </div>
+      <div class="right">
+        <img src="~@/assets/img/profile/yjt.png" alt="">
+      </div>
+    </a>
+  </div>
 </template>
 
 <script>
-export default {
-  name: "UserInfo",
-};
+  ''
+  export default {
+    name: "UserInfo",
+  };
 </script>
 
 <style scoped>
+  #user-info {
+    height: 90px;
+    background-color: var(--color-tint);
+  }
+
+  .user-info_wrap {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 10px;
+  }
+
+  .left {
+    display: flex;
+    align-items: center;
+  }
+
+  .left-img img {
+    width: 80px;
+  }
+
+  .left-text {
+    display: flex;
+    flex-direction: column;
+    margin-left: 10px;
+    border-radius: 50%;
+    color: #fff;
+    font-size: 20px;
+  }
+
+  .left-text span:nth-child(2) {
+    font-size: 14px;
+    margin-top: 5px;
+  }
+
+  .shouji {
+    display: flex;
+    align-items: flex-start;
+  }
+
+  .shouji img {
+    width: 25px;
+    color: #fff;
+  }
+
+  .right {
+    color: #fff;
+  }
+
+  .right img {
+    width: 20px;
+  }
 </style>
